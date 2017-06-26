bff72dab6192a7ddaa2296ad3c84773f47a7b0ed
kruthi-mahesh <mkruthi1997@gmail.com>
2017-06-26 15:01:07 +0530

    BOOK Mybook

---
+++ abstract1.py
@@ -0,0 +1,16 @@
+from abc import ABCMeta, abstractmethod
+class Book(object, metaclass = ABCMeta):
+	def __init__(self,title,author):
+		self.title = title
+		self.author = author
+	@abstractmethod
+	def display():
+		pass
+class MyBook(Book):
+	def __init__(self,title,author,price):
+		Book.__init__(self,title,author)
+		self.price = price
+	def display(self):
+		print('Title: ' + self.title)
+		print('Author: ' + self.Author)
+		print('Price: ' + str(self.price))
